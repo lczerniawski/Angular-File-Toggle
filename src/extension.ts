c6fc659aacaece72f9b8bdb1c4c553a8e1d46bbd
Lukasz Czerniawski <l.czerniawski@icloud.com>
2024-12-11 19:52:00 +0100

    Change commands name

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	// TODO Have shortcuts configurable
 	const toggleTs = vscode.commands.registerCommand('angular-file-toggle.toggleTs', () => {
 		toggleFile('ts');
 	});
